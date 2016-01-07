eb74cb1ab2094b18fef89dfc539919675803c068
Pawel Chojnacki <pawel@chojnacki.ws>
2016-01-07 03:46:10 +0100

    something working

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <mpi.h>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include <boost/format.hpp>
 
 #include <iostream>
 #include <random>
 #include <vector>
 
-#define MAT_LENGTH 10
-#define MAT_WIDTH 10
+#define MAT_LENGTH 10000
+#define MAT_WIDTH 1000
+
+class substr {
+
+    int sx, sy, x,y;
+public:
+    substr(int sx, int sy, int x, int y): sx(sx), sy(sy), x(x), y(y){}
+    boost::basic_format<char> toString() const {
+        return boost::format("[%1%, %2%, %3%, %4% | %5%, %6% ]") % sx % sy % x % y % (x - sx) % (y - sy);
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, substr const &m) {
+    return os << m.toString();
+}
 
 bool cool(char x){
     if (x == '1'){
@@ -16,12 +31,13 @@ bool cool(char x){
         return false;
     }
 }
+
 bool isWithinRange(boost::numeric::ublas::matrix<char> &mat, int x, int y){
     return x >= 0 && y >= 0 && x < mat.size1() && y < mat.size2();
 }
 
 int yVector(int sx, int sy, int x, int y){
-    if (y - sy == 0){
+    if ((y - sy) == 0){
         return 0;
     } else if (y - sy > 0) {
         return 1;
@@ -29,22 +45,37 @@ int yVector(int sx, int sy, int x, int y){
         return -1;
     }
 }
-
-void dupa(boost::numeric::ublas::matrix<char> &mat, int sx, int sy, int x, int y){
-    if (isWithinRange(mat, x, y) && cool(mat(x, y))){
-        int next_x = x+1, next_y = x + yVector(sx, sy, x, y);
-
-        if (isWithinRange(mat, next_x, next_y)){
-            dupa(mat, sx, sy, next_x, next_y);
-        }
+void processResult(std::vector<substr> &result, int sx, int sy, int x, int y){
+    int length = x - sx;
+    if (length >= 2){
+        result.push_back(substr(sx, sy, x, y));
     }
 }
 
-void handle(boost::numeric::ublas::matrix<char> &mat, int x, int y){
+bool dupa(boost::numeric::ublas::matrix<char> &mat, std::vector<substr> &result, int sx, int sy, int x, int y){
+//    std::cout << boost::format("--| %1% %2% %3% %4% \n") % sx % sy % x % y ;
+    bool finished = false;
+    if (isWithinRange(mat, x, y)) {
+        if (cool(mat(x, y))){
+            int next_x = x+1, next_y = y + yVector(sx, sy, x, y);
+            if (dupa(mat, result, sx, sy, next_x, next_y)){
+                processResult(result, sx, sy, x, y);
+            }
+        } else {
+            finished = true;
+
+        }
+    } else {
+        finished = true;
+    }
+    return finished;
+}
+
+void handle(boost::numeric::ublas::matrix<char> &mat, std::vector<substr> &result, int x, int y){
     if (isWithinRange(mat, x, y) && cool(mat(x,y))){
-        dupa(mat, x, y, x+1, y-1);
-        dupa(mat, x, y, x+1, y);
-        dupa(mat, x, y, x+1, y+1);
+        dupa(mat, result, x, y, x+1, y-1);
+        dupa(mat, result, x, y, x+1, y);
+        dupa(mat, result, x, y, x+1, y+1);
     }
 }
 
@@ -58,6 +89,8 @@ int main(/*int argc, char **argv*/)
 
     e2.seed(10);
 
+    std::vector<substr> result;
+
     for(auto i = 0; i < mat.size1(); i++){
         for(auto j = 0; j < mat.size2(); j++){
             mat(i,j) = dist(e2);
@@ -66,18 +99,18 @@ int main(/*int argc, char **argv*/)
 
     for(auto i = 0; i < mat.size1(); i++){
         for(auto j = 0; j < mat.size2(); j++){
-            handle(mat, i, j);
+            handle(mat, result, i, j);
         }
     }
 
+//    std::cout << mat;
+//    std::cout << "\n";
+//    for (const auto i: result){
+//      std::cout << i << '\n';
+//    }
+//    std::cout << "\n";
 
 
-    std::cout << mat;
-
-    std::cout << "\n";
-
-    auto dupa = "asf";
-    std::cout <<dupa;
 
     return 0;
 }
